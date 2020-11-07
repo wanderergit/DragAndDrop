5b4dc5749b31b971597bf44fbc9b6701a4f2af6a
Gokul Madhusudhan <gokul99.gm@gmail.com>
2020-11-07 20:52:44 +0530

    upload a single file or image

--- backend/routes/image.js
+++ backend/routes/image.js
@@ -10,3 +10,33 @@ connect.once('open', () => {
         bucketName: "uploads"
     });
 });
+
+imageRouter.route('/').post(upload.single('file'), (req, res, next) => {
+    console.log(req.body);
+    Image.findOne({ caption: req.body.caption })
+        .then((image) => {
+            console.log(image);
+            if (image) {
+                return res.status(200).json({
+                    success: false,
+                    message: 'Image already exists',
+                });
+            }
+
+            let newImage = new Image({
+                caption: req.body.caption,
+                filename: req.file.filename,
+                fileId: req.file.id,
+            });
+
+            newImage.save()
+                .then((image) => {
+                    res.status(200).json({
+                        success: true,
+                        image,
+                    });
+                })
+                .catch(err => res.status(500).json(err));
+        })
+        .catch(err => res.status(500).json(err));
+})
